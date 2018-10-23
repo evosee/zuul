cb953a44b2b125768eb5bf245edd79163d48740f
chensai <chensai123>
2018-10-23 18:33:35 +0800

    my

--- src/main/java/com/example/demo/zuul/filter/RouteFilter.java
+++ src/main/java/com/example/demo/zuul/filter/RouteFilter.java
@@ -10,7 +10,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.REQUEST_URI_KEY;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SERVICE_ID_KEY;
 
 
 @Component
@@ -40,9 +44,12 @@ public class RouteFilter extends ZuulFilter {
         if(url.indexOf("a/feignProvider")!=-1){
             String urla = "http://serviceb/b/first";
 
-            ResponseEntity<String> responseEntity = restTemplate.exchange(urla, HttpMethod.GET, null, String.class);
+
+                requestContext.set(SERVICE_ID_KEY,"serviceb");
+            requestContext.set(REQUEST_URI_KEY,"/b/first");
+            /*ResponseEntity<String> responseEntity = restTemplate.exchange(urla, HttpMethod.GET, null, String.class);
             System.out.println(responseEntity.getBody());
-            requestContext.setSendZuulResponse(false);
+            requestContext.setSendZuulResponse(false);*/
             // return responseEntity;
         }
         return null;
